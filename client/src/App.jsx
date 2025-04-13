e9ee89bac4e7106925c75bc38f9a7ca5f55b7485
Ken <122612391+pxiaccount@users.noreply.github.com>
2025-04-13 08:05:34 +0700

    Simple Post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/')
+        setData(response.data)
+      } catch (error) {
+        console.error('Error: ', error)
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <>
-
+      {data.map((x) => (
+        <div key={x.id}>
+          <h2>{x.title}</h2>
+          <p>{x.description}</p>
+        </div>
+      ))}
     </>
   )
 }
